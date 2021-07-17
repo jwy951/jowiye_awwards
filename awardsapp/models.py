67cac550be691a322bc25504436d88e028d7e608
Santa claus <santa@nortpole.com>
2021-07-17 17:50:21 +0300

    add model class profile

--- awardsapp/models.py
+++ awardsapp/models.py
@@ -1,3 +1,17 @@
+ -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.db.models.signals import post_save
+from django.dispatch  import receiver
+from django.http import Http404
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics/')
+    bio = models.CharField(max_length=255, blank=True)
