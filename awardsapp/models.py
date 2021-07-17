d4349abd1ad7258e9267835b9039908f44d3d5d4
Santa claus <santa@nortpole.com>
2021-07-17 17:51:09 +0300

    add model fuction str

--- awardsapp/models.py
+++ awardsapp/models.py
@@ -15,3 +15,9 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics/')
     bio = models.CharField(max_length=255, blank=True)
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    class Meta:
+        db_table = 'profile'
