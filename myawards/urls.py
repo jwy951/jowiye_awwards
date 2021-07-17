e4b28ffc2f9d939dd1de55509f6264df6dc9ba53
Santa claus <santa@nortpole.com>
2021-07-17 20:05:18 +0300

    add add urls

--- myawards/urls.py
+++ myawards/urls.py
@@ -21,5 +21,5 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myawards.urls')),
+    path('', include('awardsapp.urls')),
 ]
